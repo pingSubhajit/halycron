b6d83d63aa5d32eb99235147c03782b83c65b267
Subhajit Kundu <subha60kundu@gmail.com>
2025-02-20 22:09:59 +0530

    attempt: Initial fix of version mismatch

---
+++ apps/mobile/index.js
@@ -0,0 +1,2 @@
+import "expo-router/entry"
+import "../../packages/ui/src/styles/globals.css"
---
+++ apps/mobile/tailwind.config.js
@@ -0,0 +1,16 @@
+import baseConfig from "@halycon/ui/tailwind.config";
+
+/** @type {import('tailwindcss').Config} */
+const config = {
+  content: [
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "../../packages/ui/src/**/*.{js,jsx,ts,tsx}",
+  ],
+  presets: [require("nativewind/preset")],
+  theme: {
+    ...baseConfig.theme,
+  },
+  plugins: [...(baseConfig.plugins || [])],
+};
+
+export default config;
