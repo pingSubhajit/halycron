52adedb4f053971f5e098601f62170232497e780
Subhajit Kundu <subha60kundu@gmail.com>
2025-02-25 15:16:48 +0530

    feat: update album metadata generation to include album name

--- apps/web/app/app/albums/[id]/page.tsx
+++ apps/web/app/app/albums/[id]/page.tsx
@@ -1,5 +1,7 @@
 import {SingleAlbumView} from '@/app/app/albums/[id]/single-album-view'
 import type {Metadata} from 'next'
+import {getAlbumWithPhotoCount} from '@/app/api/albums/utils'
+import {notFound} from 'next/navigation'
 
 type Props = {
 	params: Promise<{ id: string }>
@@ -8,23 +10,18 @@ type Props = {
 
 export const generateMetadata = async ({params}: Props): Promise<Metadata> => {
 	const id = (await params).id
-	/*
-	 * const album = await getAlbumWithPhotoCount(id)
-	 *
-	 * if (!album) notFound()
-	 *
-	 * console.log('album', album)
-	 */
+
+	const album = await getAlbumWithPhotoCount(id)
+	if (!album) notFound()
+
 	return {
-		title: `${id} – Halycron`,
-		description: `Access your private album ${id} securely with Halycron. View, manage, and organize your photos in an encrypted album. Protect sensitive content with a PIN and ensure your memories remain private.`
+		title: `${album.name} – Halycron`,
+		description: `Access your private album ${album.name} securely with Halycron. View, manage, and organize your photos in an encrypted album. Protect sensitive content with a PIN and ensure your memories remain private.`
 	}
 }
 
 const AlbumPage = async ({params}: {params: Promise<{id: string}>}) => {
 	const {id} = await params
-	console.log('id', id)
-
 	return (
 		<div>
 			<SingleAlbumView albumId={id}/>
