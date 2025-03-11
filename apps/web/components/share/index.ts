711d4fbffcc53f7f2c4cd9d867ff0e1b62c5e0d5
Subhajit Kundu <subha60kundu@gmail.com>
2025-03-11 19:38:00 +0530

    refactor: remove unused export of ShareButton in index.ts

--- apps/web/components/share/index.ts
+++ apps/web/components/share/index.ts
@@ -1,4 +1,3 @@
-export {ShareButton} from './share-button'
 export {ShareMenuItem} from './share-menu-item'
 export {ShareDialog} from '../share-dialog'
 export {SharePinDialog} from '../share-pin-dialog'
